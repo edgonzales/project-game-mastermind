ce681b544d0fa897589c2aebe8e6ff60870abd1d
Ed Gonzales <edgonzales6@gmail.com>
2023-10-07 19:52:00 -0700

    App.js basic setup

--- js/app.js
+++ js/app.js
@@ -1,30 +1,87 @@
   /*----- CONSTANTS -----*/
-/*
+
 const MAX_GUESSES = 10; // 1 guess = a set of 4 pegs
 const MAX_PEGS_PER_GUESS = 4; // guess = single row filled with pegs
-
-*/
+const PEG_COLORS = {
+    red: 1,
+    pink: 2, 
+    green: 3,
+    blue: 4,
+    purple: 5,
+    yellow: 6
+}
 
   /*----- STATE VARIABLES -----*/
 /*
-- guessesSquares: array[null, ..., null]
-- clueSquares: array[null, ..., null]
+- DONE. guessesBoardState: array[null, ..., null]
+- DONE. clueSquares: array[null, ..., null]
 - outcome: array[null, win, loose]
-- solution: array = []
-- check button, since this will be enabled only if a guess is complete 
+- DONE. solution: array = []
+- DONE. check button, since this will be enabled only if a guess is complete 
 (4 pegs selected in 1 row): boolean false
 */
+let guessesBoardState = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+]
+
+let clueSquares = [
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+    null, null,
+]
+
+
+let solution; // random set of pegs the user is trying to guess
+
+let enableCheckButton = false; // may not need this??
 
 
   /*----- CACHED ELEMENTS  -----*/
 /*
-- pegs
-- guesses squares
-- clue squares
-- solution pegs
-- ... add all buttons
-
+- DONE. gameboard
+- DONE. peg btns
+- DONE. guesses squares
+- DONE. clue squares
+- DONE. solution pegs
+- DONE. Check button
+- DONE. Clear button
+- DONE. Reset Game button
+- selected pegs???
 */
+const gameBoard = document.querySelector('#game-board');
+const pegs = document.querySelectorAll('.peg-btn');
+const checkGuessBtn = document.querySelector('#checkGuessBtn');
+const clearGuessBtn = document.querySelector('#clearGuessBtn');
+const resetGameBtn = document.querySelector('#resetGameBtn');
+const guessesGridSquares = document.querySelectorAll('#guesses-grid tr td'); // I may need selected pegs
+const cluesGridSquares = document.querySelectorAll('#clues-grid tr td');
+const solutionsGridSquares = document.querySelectorAll('#solutions-grid tr td');
 
   /*----- EVENT LISTENERS -----*/
 /*
@@ -32,14 +89,24 @@ const MAX_PEGS_PER_GUESS = 4; // guess = single row filled with pegs
   - click on any peg 'square' ... for now
   - click on check button
   - click on clear button
-  - clck on reset game button 
+  - clck on reset game button
 */
 
+// pegs.addEventListener('click', handlePegsSelection);
+
+
   /*----- FUNCTIONS -----*/
-/*
-init(){
-    - call initial state of all variables
+
+init()
+
+function init(){
+    guessesBoardState;
+    clueSquares;
+    secretSolution; //<----- work on next
+    // solution = 
 }
+
+/*
 
 handlePegSelection()
   
@@ -82,6 +149,18 @@ renderSolution()
 
 */
 /*
+~~~~~~~~~QUESTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+~~~~~~~~~TO-DO~~~~~~~~~~~~~~~~~~~~~~~~~~
+- CSS
+    - freeze cells when btn/img changes
+        - option: don't use btns, simply change the color of the cell/square
+    - Outcome to the bottom of the screen
+    - Check btn, Clear btn, Reset Game btn display as a column
+        - display: flex | flex-flow: column?
+    - Non header text should be larger
+-
 
 
 ~~~~~~~~~~~USER STORIES~~~~~~~~~~~~~~~~~~
